2d32c44fc8dc7ce99cfb25fc19e2c8fca8864a99
Dlurak <84224239+Dlurak@users.noreply.github.com>
2024-08-25 19:26:01 +0200

    Change the appereance of the primary button

--- src/lib/components/buttons/PrimaryButton.svelte
+++ src/lib/components/buttons/PrimaryButton.svelte
@@ -10,17 +10,19 @@
 <button
 	{disabled}
 	class="
-		w-full rounded-md py-1 text-white shadow-lg transition-all
-		hover:scale-105 hover:shadow-2xl
-		focus-visible:scale-105 focus-visible:shadow-2xl
+		w-full rounded-md py-2 shadow-md
+		transition-all hover:shadow-lg hover:brightness-110
 		active:scale-95
-		disabled:cursor-not-allowed disabled:opacity-50 disabled:shadow-none
-		disabled:hover:scale-105 disabled:hover:shadow-2xl
+		disabled:cursor-not-allowed disabled:opacity-50
 	"
-	class:bg-emerald-500={color === 'default'}
-	class:bg-green-500={color === 'green'}
-	class:bg-yellow-500={color === 'yellow'}
-	class:bg-red-500={color === 'red'}
+	class:bg-emerald-400={color === 'default'}
+	class:bg-green-400={color === 'green'}
+	class:bg-yellow-400={color === 'yellow'}
+	class:bg-red-400={color === 'red'}
+	class:dark:bg-emerald-500={color === 'default'}
+	class:dark:bg-green-500={color === 'green'}
+	class:dark:bg-yellow-500={color === 'yellow'}
+	class:dark:bg-red-500={color === 'red'}
 	on:click={() => dispatch('click')}
 >
 	<slot />
--- src/lib/components/home/Box.svelte
+++ src/lib/components/home/Box.svelte
@@ -10,12 +10,13 @@
 </script>
 
 <div
-	class="flex h-full flex-col rounded-md bg-black bg-opacity-20 p-5 shadow-md transition-all hover:scale-[1.01] hover:shadow-lg print:shadow-none"
+	class="flex h-full flex-col rounded-md bg-zinc-200 p-5 shadow-md transition-all hover:scale-[1.01] hover:shadow-lg dark:bg-zinc-900 print:shadow-none"
 	class:scale-95={disfoccused}
-	class:brightness-50={disfoccused}
+	class:brightness-95={disfoccused}
+	class:dark:brightness-70={disfoccused}
 >
 	<div class="py-2 print:hidden">
-		<span class="rounded-sm bg-black bg-opacity-20 p-2 text-xl">{emoji}</span>
+		<span class="rounded-sm bg-zinc-300 p-2 text-xl dark:bg-zinc-800">{emoji}</span>
 	</div>
 	<h3><Store store={title} /></h3>
 	<Store store={description} />
