41a4a23b822f96b5376d73be51ee34d3e605ac15
Paul Schrimpf <140179344+Plurak@users.noreply.github.com>
2025-08-19 15:20:31 +0200

    fix(de.ts): correct timetable confirmation message (#9)

--- src/lib/locales/de.ts
+++ src/lib/locales/de.ts
@@ -547,7 +547,7 @@ const de = {
 	'settings.timetable.import.success': 'Stundenplan erfolgreich importiert',
 	'settings.timetable.import.error':
 		'Die Datei konnte nicht importiert werden. Enthält sie einen gültigen Stundenplan?',
-	'settings.timetable.confirm.desc': 'Möchtest du wirklich deine bisherigen Farben überschreiben?',
+	'settings.timetable.confirm.desc': 'Möchtest du wirklich deinen bisherigen Stundenplan überschreiben?',
 	'settings.timetable.showWeekend': 'Zeige das Wochende im Editor an',
 
 	'settings.general.holiday': 'Ferien & Feiertage',
